4137006ea4bf309c5849d3c241eb03a9da07f192
Avinash B <a4avinash@hotmail.com>
2021-12-16 00:32:19 +0000

    Updated comments

--- app/Main.hs
+++ app/Main.hs
@@ -10,13 +10,12 @@ import Types
 import Text.Show.Unicode
 import Main.Utf8 (withUtf8)
 import Types
-import Types (Record(england_and_wales))
 
 main :: IO ()
 main = withUtf8 $ do
     -- Initialises DB and Creates Tables If not exists already
     conn <- initialiseDB
-    -- url for JSON data for bank holidays in UK
+    -- url for JSON data for bank holidays in UK from 2017 to 2023
     let url = "https://www.gov.uk/bank-holidays.json"
     print "Downloading..."
     json <- download url
